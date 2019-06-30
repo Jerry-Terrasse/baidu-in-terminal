c33573269e93fa6594dda7d7a92d896b384c07c5
Terrasse <3305049949@qq.com>
2019-06-30 21:25:52 +0800

    Fixed bugs

--- main.py
+++ main.py
@@ -17,53 +17,53 @@ work = wd.Firefox(options=ff_options)
 
 
 def main():
+    global work,step,limit,page_step
     kw = ' '.join(sys.argv[1:])
-#    print(kw)
     work.get(baidu)
-#    time.sleep(1)
     work.find_element_by_id("kw").send_keys(kw)
     work.find_element_by_id("su").click()
     home = work.current_window_handle
     i = 1
+    time.sleep(1)
     while True:
-        for id in range(i, i+step):
-            try:
-                time.sleep(1)
-                print('\033[34m' + str(id) + '\033[0m')
-#                input("pause")
-                print('\033[1m' + work.find_element_by_id(str(id)).find_element_by_class_name("t").text + '\033[0m')
-#                input("pause")
-#                time.sleep(0.5)
-                print(work.find_element_by_id(str(id)).find_element_by_class_name("c-abstract").text)
-#                input("pause")
-#                time.sleep(0.5)
-            except:
-                print(null)
-                continue
-        i += step
-        ln = input(">>>")
-        try:
-            cmd = eval(ln)
-            work.find_element_by_id(str(cmd)).find_element_by_class_name("t").find_element_by_tag_name("a").click()
-            handles = work.window_handles
-            for handle in handles:
-                if handle != home:
-                    work.switch_to_window(handle)
-                    break
-            print('\033[1m' + work.title + '\033[0m')
-            print(work.find_element_by_tag_name("html").text)
-            exit()
-        except:
-            continue
         if i > limit:
-#            work.get_element_by_id("page").get_element_by_tag_name("strong").find_element_by_xpath("")
+            print("Turning to next page...")
             work.find_element_by_xpath("//div[@id='page']/strong/following-sibling::a[1]").click()
             limit += page_step
+            time.sleep(1)
+        for id in range(i, i+step):
+            try:
+                print('\033[34m' + str(id) + '\033[0m')
+                div = work.find_element_by_id(str(id))
+                print('\033[1m' + div.find_element_by_xpath("h3").text+ '\033[0m')
+                print(div.find_element_by_xpath("div[1]").text)
+            except:
+                print('null')
+                continue
+        i += step
+        while True:
+            ln = input("^_^>")
+            if ln == 'exit':
+                work.quit()
+                exit()
+            try:
+                cmd = eval(ln)
+                work.find_element_by_id(str(cmd)).find_element_by_class_name("t").find_element_by_tag_name("a").click()
+                time.sleep(3)
+                work.switch_to.window(work.window_handles[1])
+                print('\033[1m' + work.title + '\033[0m')
+                print(work.find_element_by_tag_name("html").text)
+                work.close()
+                work.switch_to.window(home)
+            except:
+                break
 
 
 if __name__ == '__main__':
     try:
         main()
+    except SystemExit:
+        pass
     except:
         print("Sorry, something went wrong..")
     work.quit()
